9abf3206c44a7f7089d21fba5a441e2a874fd820
Huimin <huiminzhang0111@hotmail.com>
2021-08-23 10:37:56 -0700

    had some local change

---
+++ src/Login/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Signup() {
+    return (
+        <div className='signup'>
+            <h1>hello from sign up page</h1>
+
+        </div>
+    )
+}
