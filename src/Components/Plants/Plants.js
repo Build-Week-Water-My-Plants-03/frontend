8d571ae101946d820f4eaff90940185f25d78760
Kade Griffith <kadeg95@gmail.com>
2021-08-23 14:05:29 -0600

    added various needed components for the plants page, including: a header, create a plant form, plant list section. Set up the plant form to track user input for each category. Implemented a CSS reset into the app.js file

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -1,8 +1,36 @@
 import axios from "axios";
-import styled from "styled-components";
+import { useState, useEffect } from "react";
+import Header from "../Plants-Header/Header";
+import Plant from "../Plant/Plant";
+import PlantForm from "../PlantForm/PlantForm";
+import "./Plants.css";
 
 const Plants = () => {
-    return null;
+    const [plant, setPlant] = useState({
+        id: "",
+        nickname: "",
+        species: "",
+        h20frequency: "",
+    })
+
+    const [plantData, setPlantData] = useState([]);
+    const [reqError, setReqError] = useState(null);
+
+    useEffect( () => {
+        axios.get("fakeapi.com")
+        .then(res => setPlantData(res.data))
+        .catch( err => setReqError(err))
+    }, [])
+
+    return(
+        <div className="container">
+            <Header />
+            <PlantForm />
+            <section className="plantList" >
+                {plantData.map( (plant, index) => <Plant key={index} nickname={plant.nickname} species={plant.species} h20frequency={plant.h20frequency} />)}
+            </section>
+        </div>
+    )
 }
 
 export default Plants;
