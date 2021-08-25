6ecd411ad4736c86a3e29f263a4a1d29e75ba945
Kade Griffith <kadeg95@gmail.com>
2021-08-25 10:06:41 -0600

    deleted temporary code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import PlantList from './Components/PlantList/PlantList';
 import PlantDetails from './Components/PlantDetails/PlantDetails';
-import {Link, Switch, Route} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Switch>
       <Route exact path='/'>
-        <Link to="/plants">plants</Link>
+
       </Route>
       <Route path='/plants'>
         <PlantList />
