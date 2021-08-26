31eb96f440a5e2364c264c46be7dfaa4678d0bcf
RimshaSaleem <rimshatif8@gmail.com>
2021-08-26 05:04:18 -0700

    Done CRUD page also added Routing, Homepage and Crud page finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
-import React from "react";
+ import React from "react";
 import './App.css';
 import Plants from './components/Plants/Plants';
+import TodoList from './components/TodoList';
+
 import Plant from './components/Plant/Plant';
 import {Switch, Route} from 'react-router-dom';
 import { Homepage } from "./containers/homepage";
+
+
+import './App.css';
 function App(props) {
   return (
     <Switch>
-       return <Homepage />;
-      <Route path='/'>
-        
+       return(
+         <Route exact path='/'>
+        <Homepage />
       </Route>
+      <div className='todo-app'>
+      <Route path='/description'>
+      <TodoList />
+      </Route>
+    </div> 
       <Route path='/plants'>
         <Plants />
       </Route>
       <Route path='/plants/:id'>
         <Plant />
       </Route>
+      
     </Switch>
   );
 }
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setInput('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Update your item'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Update
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Add an Id'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <br></br>
+         <br></br>
+          <input
+            placeholder='Add a Nickname'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+         
+          <br></br>
+          <br></br>
+          <input
+            placeholder='Add a Species'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+         
+          <br></br>
+          <br></br>
+          <button onClick={handleSubmit} className='todo-button'>
+            Add Data
+          </button>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
